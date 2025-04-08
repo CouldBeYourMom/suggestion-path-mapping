056984b41e87e04995196679f14163e6c42554e0
kevinyuQ <kevin.yu@ufl.edu>
2025-04-08 16:18:33 -0400

    Laying out foundations for A* search algorithm

--- cpp/search_astar.cpp
+++ cpp/search_astar.cpp
@@ -1,6 +1,42 @@
 #include "search_astar.h"
 
+/*
+ * Input: A node in the graph.
+ * Output: The heuristic value for this node.
+ * This function should calculate the heuristic value for a node using the heuristic function.
+ *
+ */
+float SearchAStar::heuristic(const std::string& startNode) {
+
+    return 0;
+}
+
 std::vector<std::string> SearchAStar::astar(const Graph& graph, const std::string& startNode, const std::string& endNode) {
     // TODO: Implement A* Search Algorithm
-    return {};
+    std::vector<std::string> shortestPath;
+    std::priority_queue<std::string> open;
+    fValues[startNode] = 0;
+    gValues[startNode] = 0;
+
+
+    //
+    while (!open.empty()) {
+        std::string currentNode = open.top();
+        open.pop();
+
+        // Retrieve neighbors of currentNode
+        std::vector<std::string> neighbors; // = getNeighbors(currentNode)
+        // For each neighbor of currentNode.
+        for (auto n : neighbors) {
+            //
+            // Calculate tentative g value for neighbor. g(neighbor) = g(currentNode) + weight(currentNode to neighbor)
+            float tentativeG = gValues[currentNode]; // tentativeG = g(currentNode) + edge weight
+            // if tentative g is less than current g value of neighbor, perform updates.
+            if (tentativeG < gValues[n]) {
+                // Perform updates: update g(neighbor) and f(neighbor). Update parent(neighbor)
+            }
+        }
+    }
+
+    return shortestPath;
 }
--- cpp/search_astar.h
+++ cpp/search_astar.h
@@ -2,13 +2,19 @@
 #define SEARCH_ASTAR_H
 
 #include "graph.h"
+#include <queue>
+#include <unordered_map>
 #include <vector>
 #include <string>
 
 // Kevin Yu: Implementing A* search algorithm
 
 class SearchAStar {
+  // Keep track of f values and g values for each node.
+    static std::unordered_map<std::string, float> gValues;
+    static std::unordered_map<std::string, float> fValues;
 public:
+    static float heuristic(const std::string& startNode);
     static std::vector<std::string> astar(const Graph& graph, const std::string& startNode, const std::string& endNode);
 };
 
