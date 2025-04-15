1b8b0f4e18680bf72b2e9335cfdb364341dfbe14
kevinyuQ <kevin.yu@ufl.edu>
2025-04-15 16:46:15 -0400

    Added comparator for priority queue. still resolving conflict

--- cpp/search_astar.cpp
+++ cpp/search_astar.cpp
@@ -9,7 +9,9 @@
  *
  */
 float SearchAStar::heuristic(const std::string& startNode) {
-    return 0;
+
+    // Potential heuristic: number of views in the node's video
+    return 0.0;
 }
 
 /*
@@ -23,21 +25,30 @@ std::vector<std::string> SearchAStar::astar(const Graph& graph, const std::strin
     // TODO: Implement A* Search Algorithm
     std::vector<std::pair<std::string, double>> result;
     std::priority_queue<std::string> open;
+    std::vector<std::string> shortestPath;
+    // Define lambda function for comparing values in PQ.
+    // Returns true if a comes before b. However, b is the first to come out.
+    // f_a > f_b: b will come out first if it has smaller f value
+    auto cmp = [](std::string firstNode, std::string secondNode){
+        return fValues[firstNode] > fValues[secondNode];
+    };
+    // Define priority queue
+    std::priority_queue<std::string, std::vector<std::string>, decltype(cmp)> unprocessed(cmp);
     fValues[startNode] = 0;
     gValues[startNode] = 0;
 
 
     //
-    while (!open.empty()) {
-        std::string currentNode = open.top();
-        open.pop();
+    while (!unprocessed.empty()) {
+        std::string currentNode = unprocessed.top();
+        unprocessed.pop();
 
         // Retrieve neighbors of currentNode
-        std::vector<std::string> neighbors = getNeighbors(currentNode);
+        std::vector<std::string> neighbors = graph.adjList.at(currentNode);
         // For each neighbor of currentNode.
         for (auto n : neighbors) {
             // Calculate tentative g value for neighbor. g(neighbor) = g(currentNode) + weight(currentNode to neighbor)
-            float edgeWeight = 0.0; // Update this with true edge weights.
+            float edgeWeight = 0.0; // Get edge weight from current node to the neighbor.
             float tentativeG = gValues[currentNode] + edgeWeight; // tentativeG = g(currentNode) + edge weight
 
             // if tentative g is less than current g value of neighbor, perform updates.
@@ -48,7 +59,7 @@ std::vector<std::string> SearchAStar::astar(const Graph& graph, const std::strin
             }
 
             // Add neighbor to priority queue.
-            open.push(n);
+            unprocessed.push(n);
         }
     }
 
